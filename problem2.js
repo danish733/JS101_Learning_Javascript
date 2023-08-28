d8bd1c4b8387243e2ae34b20c2836e385e9d3215
Md Danish <danishreza733@gmail.com>
2023-08-28 17:59:26 +0530

    Introduction_to_javascript

---
+++ problem2.js
@@ -0,0 +1,17 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+var Name = "Md Danish"
+console.log(Name)
+
+var Father_name = "Md Matlub Reza"
+var Name = Father_name
+console.log(Name)
+
+var Mother_name = "Asmina Khatoon"
+var Name = Mother_name
+console.log(Name)
---
+++ problem3.js
@@ -0,0 +1,5 @@
+var Name = "Md Danish"
+var Age =20
+console.log(Name,Age)
+console.log(typeof(Name))
+console.log(typeof(Age))
